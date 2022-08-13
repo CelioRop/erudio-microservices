9abc0edc67ed104e92888c724dfcd7cec0e96b5c
CelioRop <celio.bhz@gmail.com>
2022-08-13 15:51:02 -0300

    aula 12/08/2022

--- 02-calculator/src/main/java/br/com/erudio/MathController.java
+++ 02-calculator/src/main/java/br/com/erudio/MathController.java
@@ -18,7 +18,54 @@ public class MathController {
 		Double sum = ConvertDouble(numberOne) + ConvertDouble(numberTwo);
 		return sum;	
 	}
+	
+	@RequestMapping(value="/sub/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double sub (@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo ) throws Exception {
+		if(!IsNumeric(numberOne) || !IsNumeric(numberTwo )){
+			throw new UnsuportedMathOperationException("Please set numeric value");
+		}
+		Double sum = ConvertDouble(numberOne) - ConvertDouble(numberTwo);
+		return sum;	
+	}
+	
+	@RequestMapping(value="/div/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double div (@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo ) throws Exception {
+		if(!IsNumeric(numberOne) || !IsNumeric(numberTwo )){
+			throw new UnsuportedMathOperationException("Please set numeric value");
+		}
+		Double sum = ConvertDouble(numberOne) / ConvertDouble(numberTwo);
+		return sum;	
+	}
+	
+	@RequestMapping(value="/mult/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double mult (@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo ) throws Exception {
+		if(!IsNumeric(numberOne) || !IsNumeric(numberTwo )){
+			throw new UnsuportedMathOperationException("Please set numeric value");
+		}
+		Double sum = ConvertDouble(numberOne) * ConvertDouble(numberTwo);
+		return sum;	
+	}
+	
+	@RequestMapping(value="/med/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double med (@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo ) throws Exception {
+		if(!IsNumeric(numberOne) || !IsNumeric(numberTwo )){
+			throw new UnsuportedMathOperationException("Please set numeric value");
+		}
+		Double sum = (ConvertDouble(numberOne) + ConvertDouble(numberTwo))/2;
+		return sum;	
+	}
 
+	@RequestMapping(value="/squa/{numberOne}", method=RequestMethod.GET)
+	public Double squa (@PathVariable("numberOne") String numberOne ) throws Exception {
+		if(!IsNumeric(numberOne)){
+			throw new UnsuportedMathOperationException("Please set numeric value");
+		}
+		Double sum = (Double)Math.sqrt(ConvertDouble(numberOne));
+		return sum;	
+	}
+	
+
+	
 	private Double ConvertDouble(String strNumber) {
 		if(strNumber == null) return 0D;
 		String number = strNumber.replaceAll(",", ".");
